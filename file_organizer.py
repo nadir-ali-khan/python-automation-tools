6d7d1b05e07a11bd9616071742b3edd21efddca7
Nadir Ali Khan <118495596+nadir-ali-khan@users.noreply.github.com>
2026-07-07 08:41:51 +0500

    feat: initial project setup

---
+++ file_organizer.py
@@ -0,0 +1,11 @@
+import os, shutil
+
+def organize(folder):
+    for f in os.listdir(folder):
+        ext = f.split(".")[-1].lower()
+        dest = os.path.join(folder, ext)
+        os.makedirs(dest, exist_ok=True)
+        shutil.move(os.path.join(folder, f), dest)
+
+if __name__ == "__main__":
+    organize(os.path.expanduser("~/Downloads"))
